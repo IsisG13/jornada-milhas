df909fa8c85522105fdc012be6a6f0235dc8e742
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2025-02-05 14:52:36 -0300

    criando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './sheared/header/header.component';
+import { FooterComponent } from './sheared/footer/footer.component';
+import { BannerComponent } from './sheared/banner/banner.component';
+import { CardComponent } from './sheared/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
